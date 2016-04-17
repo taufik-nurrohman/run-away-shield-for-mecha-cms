b8a68efba59c1e9afbaaabf595465180f8b09f46
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-04-17 18:01:14 +0700

    Updates

--- run-away/functions.php
+++ run-away/functions.php
@@ -1,19 +1,5 @@
 <?php
 
-Widget::add('tagLinks', function($connect = ', ') use($config, $speak) {
-    $links = array();
-    $article = Shield::lot('article');
-    if( ! $article || ! isset($article->tags)) return "";
-    foreach($article->tags as $tag) {
-        if($tag && $tag->id !== 0) {
-            $url = Filter::colon('tag:url', $config->url . '/' . $config->tag->slug . '/' . $tag->slug);
-            $links[] = '<a href="' . $url . '" rel="tag">' . $tag->name . '</a>';
-        }
-    }
-    $text = count($links) > 1 ? $speak->tags : $speak->tag;
-    return ! empty($links) ? $text . ': ' . implode($connect, $links) : "";
-});
-
 Filter::add('chunk:output', function($content, $path) {
     $name = File::N($path);
     if($name === 'block.footer') {
